b5706875f8c939e3303f8e691e1a798beea8c1e8
NChislom <11775893+nchislom@users.noreply.github.com>
2019-06-20 18:27:01 -0400

    CRUD operations functional --- no styling

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+    ('Bacon Burger', false),
+    ('Umami Burger', false),
+    ('Cheddar & Onion Burger', false),
+    ('Classic Burger', false);
